11aa8144b179a6139b3cea868d82cd64620c6b8e
Vytautas P <v.petrucionis@gmail.com>
2023-04-27 15:49:17 +0300

    firebase installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import HomePage from "./components/pages/HomePage";
 
 function App() {
   return (
     <div>
       <h1 className="bg-black text-white">Laba diens</h1>
+      <HomePage />
     </div>
   );
 }
---
+++ src/components/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="container mx-auto max-w-5xl">
+      <h1 className="bg-black p-5 text-white mt-5 mb-5">Home page Here</h1>
+    </div>
+  );
+}
+
+export default HomePage;
