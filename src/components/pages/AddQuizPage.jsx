9c1184dfad1a5e79bb02a4341875b9b871b5b4da
Vytautas P <v.petrucionis@gmail.com>
2023-04-28 15:44:03 +0300

    working quizform!

--- src/components/pages/AddQuizPage.jsx
+++ src/components/pages/AddQuizPage.jsx
@@ -1,30 +1,71 @@
 import React from "react";
 import { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function AddQuizPage() {
   const [category, setCategory] = useState("history");
   const [numQuestions, setNumQuestions] = useState("5");
   const [questions, setQuestions] = useState([{ question: "", answers: ["", "", "", ""], correctAnswer: -1 }]);
 
+  const initialValues = {
+    category: "history",
+    numQuestions: "5",
+    questions: Array.from({ length: 5 }, () => ({
+      question: "",
+      answers: ["", "", "", ""],
+      correctAnswer: -1,
+    })),
+  };
+
+  const validationSchema = Yup.object().shape({
+    category: Yup.string().required("Required"),
+    numQuestions: Yup.number().required("Required"),
+    questions: Yup.array().of(
+      Yup.object().shape({
+        question: Yup.string(),
+        answers: Yup.array().of(Yup.string()),
+        correctAnswer: Yup.number().min(0, "Required"),
+      })
+    ),
+  });
+
+  const formik = useFormik({
+    initialValues: initialValues,
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  const handleNumQuestionsChange = (e) => {
+    formik.setFieldValue("numQuestions", e.target.value);
+    formik.setFieldValue(
+      "questions",
+      Array.from({ length: parseInt(e.target.value) }, () => ({
+        question: "",
+        answers: ["", "", "", ""],
+        correctAnswer: -1,
+      }))
+    );
+  };
+
+  const handleCorrectAnswerChange = (questionIndex, answerIndex) => {
+    formik.setFieldValue(`questions.${questionIndex}.correctAnswer`, answerIndex);
+  };
+
   const handleCategoryChange = (e) => {
     setCategory(e.target.value);
   };
 
-  const handleNumQuestionsChange = (e) => {
-    setNumQuestions(e.target.value);
-    setQuestions(Array(parseInt(e.target.value)).fill({ question: "", answers: ["", "", "", ""], correctAnswer: -1 }));
-  };
-
   const handleQuestionChange = (questionIndex, e) => {
-    const newQuestions = [...questions];
+    const newQuestions = [...formik.values.questions];
     newQuestions[questionIndex].question = e.target.value;
-    setQuestions(newQuestions);
+    formik.setFieldValue("questions", newQuestions);
   };
 
   const handleAnswerChange = (questionIndex, answerIndex, e) => {
-    const newQuestions = [...questions];
-    newQuestions[questionIndex].answers[answerIndex] = e.target.value;
-    setQuestions(newQuestions);
+    formik.setFieldValue(`questions.${questionIndex}.answers.${answerIndex}`, e.target.value);
   };
 
   const handleSubmit = (e) => {
@@ -41,7 +82,7 @@ function AddQuizPage() {
         <div className="flex flex-col items-center px-4 pt-2 pb-5 w-full mb-10 bg-black text-white rounded-lg">
           <p className="text-center text-qxl pb-1">Please fill the new quiz form</p>
           <div className="font-cursive text-black pt-6 pb-3 px-5">
-            <form onSubmit={handleSubmit} className="bg-yellow space-y-4 space-b-10 rounded-lg p-5">
+            <form onSubmit={formik.handleSubmit} className="bg-yellow space-y-4 space-b-10 rounded-lg p-5">
               <div>
                 <label htmlFor="category" className="block text-sm font-medium text-gray-700">
                   Category
@@ -75,20 +116,19 @@ function AddQuizPage() {
                 </select>
               </div>
 
-              {questions.map((q, questionIndex) => (
+              {formik.values.questions.map((q, questionIndex) => (
                 <div key={questionIndex} className="space-y-4">
-                  <div>
-                    <label htmlFor={`question-${questionIndex}`} className="block text-sm font-medium text-gray-700">
-                      Question {questionIndex + 1}
-                    </label>
-                    <input
-                      id={`question-${questionIndex}`}
-                      value={q.question}
-                      onChange={(e) => handleQuestionChange(questionIndex, e)}
-                      type="text"
-                      className="mt-1 w-full py-2 px-3 border border-black bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    />
-                  </div>
+                  <label htmlFor={`question-${questionIndex}`} className="block text-sm font-medium text-gray-700">
+                    Question {questionIndex + 1}
+                  </label>
+                  <input
+                    id={`question-${questionIndex}`}
+                    value={q.question}
+                    onChange={(e) => handleQuestionChange(questionIndex, e)}
+                    type="text"
+                    className="mt-1 w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:border-indigo-500 sm:text-sm"
+                  />
+
                   <div className="flex flex-wrap">
                     {q.answers.map((a, answerIndex) => (
                       <div key={answerIndex} className="w-1/4 pr-2">
@@ -100,19 +140,15 @@ function AddQuizPage() {
                           value={a}
                           onChange={(e) => handleAnswerChange(questionIndex, answerIndex, e)}
                           type="text"
-                          className="mt-1 w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:border-indigo-500 sm:text-sm"
+                          className="mt-1 w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:border-black sm:text-sm"
                         />
                         <input
                           type="radio"
                           id={`correctAnswer-${questionIndex}-${answerIndex}`}
-                          name={`correctAnswer-${questionIndex}`}
+                          name={`questions.${questionIndex}.correctAnswer`}
                           value={answerIndex}
                           checked={q.correctAnswer === answerIndex}
-                          onChange={() => {
-                            const newQuestions = [...questions];
-                            newQuestions[questionIndex].correctAnswer = answerIndex;
-                            setQuestions(newQuestions);
-                          }}
+                          onChange={() => handleCorrectAnswerChange(questionIndex, answerIndex)}
                           className="mt-1"
                         />
                         <label htmlFor={`correctAnswer-${questionIndex}-${answerIndex}`} className="block text-sm font-medium text-gray-700">
