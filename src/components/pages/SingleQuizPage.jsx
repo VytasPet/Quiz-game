b00eea9c3388725c5a81daddfe8e797c40c49f29
Vytautas P <v.petrucionis@gmail.com>
2023-05-01 11:14:24 +0300

    signle quiz page

--- src/components/pages/SingleQuizPage.jsx
+++ src/components/pages/SingleQuizPage.jsx
@@ -1,99 +1,102 @@
-import React from "react";
-import QuizForm from "../forms/QuizForm";
+import React, { useState } from "react";
+import { useFormik } from "formik";
 
-function SingleQuizPage() {
+function QuizPage() {
+  // Example quiz data
+  const quizData = {
+    title: "Example Quiz",
+    category: "History",
+    questions: [
+      {
+        question: "What year did Christopher Columbus discover America?",
+        answers: ["1492", "1776", "1865", "1969"],
+        correctAnswer: 0,
+      },
+      {
+        question: "Who was the first president of the United States?",
+        answers: ["George Washington", "Thomas Jefferson", "John Adams", "Abraham Lincoln"],
+        correctAnswer: 0,
+      },
+      {
+        question: "What is the capital of France?",
+        answers: ["London", "Paris", "Berlin", "Rome"],
+        correctAnswer: 1,
+      },
+    ],
+  };
+
+  const [userAnswers, setUserAnswers] = useState(Array(quizData.questions.length).fill(-1));
+  const [error, setError] = useState("");
+
+  const handleAnswerChange = (questionIndex, answerIndex) => {
+    const newUserAnswers = [...userAnswers];
+    newUserAnswers[questionIndex] = answerIndex;
+    if (!newUserAnswers.includes(-1)) {
+      setError("");
+    }
+    setUserAnswers(newUserAnswers);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      userAnswers: userAnswers,
+    },
+    onSubmit: () => {
+      setError("");
+      const hasUnansweredQuestions = userAnswers.includes(-1);
+      if (hasUnansweredQuestions) {
+        setError("Please answer all questions.");
+        return;
+      }
+      console.log(userAnswers);
+      // TODO: Submit user answers to server
+    },
+  });
+
+  const { handleSubmit } = formik;
+
   return (
     <div className="mt-20 box-border">
-      <h1 className="text-5xl mb-20">2023 NBA season statistics</h1>
+      <h1 className="text-5xl mb-20">{quizData.title}</h1>
+      <h2 className="text-2xl mb-10">{quizData.category}</h2>
 
-      <div className="flex flex-col items-center">
-        <div className="flex flex-col items-center px-4 pt-2 pb-5 w-full mb-10 bg-black text-white rounded-lg">
-          <div className="font-cursive text-black pt-6 pb-3 px-5"></div>
-          <form onSubmit={formik.handleSubmit} className="bg-yellow space-y-4 space-b-10 rounded-lg p-5">
+      <form onSubmit={handleSubmit} className="bg-yellow space-y-4 space-b-10 rounded-lg p-5">
+        {quizData.questions.map((q, questionIndex) => (
+          <div key={questionIndex} className="space-y-4">
             <div>
-              <label htmlFor="category" className="block text-sm font-medium text-gray-700">
-                Category
+              <label htmlFor={`question-${questionIndex}`} className="block text-lg font-medium text-gray-700">
+                {q.question}
               </label>
-              <select
-                id="category"
-                value={category}
-                onChange={handleCategoryChange}
-                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              >
-                <option value="history">History</option>
-                <option value="sports">Sports</option>
-                <option value="geography">Geography</option>
-              </select>
             </div>
-
-            <div>
-              <label htmlFor="num_questions" className="block text-sm font-medium text-gray-700">
-                Number of Questions
-              </label>
-              <select
-                id="num_questions"
-                value={numQuestions}
-                onChange={handleNumQuestionsChange}
-                className="mt-1 block w-full py-2 px-3 border border-black bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              >
-                <option value="5">5</option>
-                <option value="10">10</option>
-                <option value="15">15</option>
-                <option value="20">20</option>
-              </select>
-            </div>
-
-            {formik.values.questions.map((q, questionIndex) => (
-              <div key={questionIndex} className="space-y-4">
-                <label htmlFor={`question-${questionIndex}`} className="block text-sm font-medium text-gray-700">
-                  Question {questionIndex + 1}
-                </label>
-                <input
-                  id={`question-${questionIndex}`}
-                  value={q.question}
-                  onChange={(e) => handleQuestionChange(questionIndex, e)}
-                  type="text"
-                  className="mt-1 w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:border-indigo-500 sm:text-sm"
-                />
-
-                <div className="flex flex-wrap">
-                  {q.answers.map((a, answerIndex) => (
-                    <div key={answerIndex} className="w-1/4 pr-2">
-                      <label htmlFor={`answer-${questionIndex}-${answerIndex}`} className="block text-sm font-medium text-gray-700">
-                        Answer {answerIndex + 1}
-                      </label>
-                      <input
-                        id={`answer-${questionIndex}-${answerIndex}`}
-                        value={a}
-                        onChange={(e) => handleAnswerChange(questionIndex, answerIndex, e)}
-                        type="text"
-                        className="mt-1 w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:border-black sm:text-sm"
-                      />
-                      <input
-                        type="radio"
-                        id={`correctAnswer-${questionIndex}-${answerIndex}`}
-                        name={`questions.${questionIndex}.correctAnswer`}
-                        value={answerIndex}
-                        checked={q.correctAnswer === answerIndex}
-                        onChange={() => handleCorrectAnswerChange(questionIndex, answerIndex)}
-                        className="mt-1"
-                      />
-                      <label htmlFor={`correctAnswer-${questionIndex}-${answerIndex}`} className="block text-sm font-medium">
-                        Correct
-                      </label>
-                    </div>
-                  ))}
+            <div className="flex flex-wrap">
+              {q.answers.map((a, answerIndex) => (
+                <div key={answerIndex} className="w-1/4 pr-2">
+                  <label htmlFor={`answer-${questionIndex}-${answerIndex}`} className="block text-md font-medium text-gray-700">
+                    {a}
+                  </label>
+                  <input
+                    id={`answer-${questionIndex}-${answerIndex}`}
+                    type="radio"
+                    name={`answer-${questionIndex}`}
+                    value={answerIndex}
+                    checked={userAnswers[questionIndex] === answerIndex}
+                    onChange={() => handleAnswerChange(questionIndex, answerIndex)}
+                    className="mt-1"
+                  />
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
+          </div>
+        ))}
 
-            <button type="submit" className="bg-black mt-10 text-white py-2 px-4 rounded hover:bg-background hover:text-black hover:outline ">
-              Create Quiz
-            </button>
-          </form>
-        </div>
-      </div>
+        {error && <div className="text-red-500">{error}</div>}
+
+        <button type="submit" className="bg-black hover:bg-blue-700 text-white py-2 px-4 rounded">
+          Submit Answers
+        </button>
+      </form>
     </div>
   );
 }
-export default SingleQuizPage;
+
+export default QuizPage;
