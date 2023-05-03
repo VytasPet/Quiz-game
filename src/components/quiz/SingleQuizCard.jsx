f3957dcebd4355da1afc20d1d0ff12e9c491e69b
Vytautas P <v.petrucionis@gmail.com>
2023-05-03 14:04:24 +0300

    hover

--- src/components/quiz/SingleQuizCard.jsx
+++ src/components/quiz/SingleQuizCard.jsx
@@ -12,7 +12,7 @@ function SingleQuizCard({ item }) {
       <div className="group flex flex-col items-center px-4 pt-2 pb-5 w-2/3 mb-10 bg-black text-white rounded-full">
         <p className=" text-qxl pb-1">{item.category.stringValue}</p>
         <div className="bg-yellow font-cursive text-black pt-6 pb-3 px-5 rounded-full w-full hover:bg-background">
-          <h2 className="bg-background uppercase text-center p-2 rounded-full inline group-hover:bg-yellow">{item.name.stringValue}</h2>
+          <h2 className="bg-black text-white uppercase text-center p-2 rounded-full inline ">{item.name.stringValue}</h2>
           <p className="font-cursive my-5">Questions: {item.numQuestions.stringValue}</p>
           <p className="font-sans">Average result: {item?.completed.integerValue > 0 ? (item?.results.integerValue / item?.completed.integerValue).toFixed(2) : 0}%</p>
         </div>
