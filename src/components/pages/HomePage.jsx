c9cc0414e2f0f7c1d2f2344ec102770f084cfb98
Vytautas P <v.petrucionis@gmail.com>
2023-05-23 09:30:04 +0300

    register+signupmenu

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -40,7 +40,7 @@ function HomePage() {
   return (
     <div className="flex flex-col items-center">
       <div className="container flex justify-around mx-auto mt-10 max-w-5xl flex-row max-sm:mt-1 max-sm:flex-col">
-        <h1 className="p-5 text-6xl text-black leading-normal mt-5 mb-5 text-center w-2/5 max-sm:text-4xl max-sm:w-full max-sm:mb-1">Welcome to the greatest Quiz application</h1>
+        <h1 className="p-5 text-6xl text-white leading-normal mt-5 mb-5 text-center w-2/5 max-sm:text-4xl max-sm:w-full max-sm:mb-1">Welcome to the greatest Quiz application</h1>
         {sortArr && (
         <div className="w-1/2 right items-center border-none  rounded-lg font-bold mt-5 text-white max-sm:w-full max-sm:mt-10">
           <p className="my-6">Leaderboard</p>
@@ -49,33 +49,64 @@ function HomePage() {
           <p className="py-3 w-1/3 bg-blue rounded-[13px]">Week</p>
           <p className="py-3 w-1/3 text-grey rounded-[13px]">Month</p>
           </div>
-          <p>labas</p>
-          <div className="flex justify-around  gap-5 bg-yellow text-black w-full rounded-lg ">
-          <ul className="flex items-end justify-end">
+          <div className="flex justify-around pt-7 gap-5 bg-yellow text-black w-full rounded-lg ">
+          <ul className="flex items-end max-w-full max-h-full justify-end max-sm:mt-10 max-md: text-xs">
   {value &&
     sortArr.slice(0, 3).map((obj, index) => (
       <li
         key={obj.userUid}
-        className={`max-sm:text-sm flex flex-col text-white font-medium w-1/3 w-max-full text-lg justify-end bg-[url('src/assets/images/1stStep.svg')] bg-cover rounded-t-3xl ${
+        className={`max-sm:text-sm  flex flex-col text-white font-medium w-1/3 w-max-full text-lg justify-end bg-cover rounded-t-3xl ${
           index === 0 ? 'h-52' : 'h-40'
         }`}
         style={{ order: index === 1 ? -1 : index }}
       >
         {index + 1}.{' '}
         {index + 1 === 3 ? (
+
+                      <div className="relative flex items-end w-full justify-center">
+          <div className="absolute flex flex-col w-full max-w-full items-center">
+            <div className="absolute max-w-[60%] max-sm:top-[-30%]">
+            <img className="absolute bottom-[-10%] left-[40%]" src="src/assets/images/secPL.svg" alt="" />
+          <img className=""  src="src/assets/images/happyWinner.svg" alt="" />
+            </div>
+          <img className="w-4/5" src="src/assets/images/2-3Place.svg" w-full alt="lab" />
+          </div>
           <div>
             {obj.email}
             <p>trecias</p>
           </div>
+          </div>
         ) : index + 1 === 1 ? (
-          <div>
-            {obj.email}
+          <div className="relative flex items-end w-full justify-center">
+            <div className="absolute flex flex-col w-full items-center ">
+            <div className="absolute max-w-[70%] max-sm:top-[-30%] flex flex-col items-center">
+              <div >
+               <img className="absolute top-[-40%] left-[30%] max-sm:top-[-40%]" src="src/assets/images/crownking.svg" alt="" />
+               <img  src="src/assets/images/happyWinner.svg"  alt="" />
+              </div>
+              <img className="absolute bottom-[-10%] left-[35%]"  src="src/assets/images/firstPL.svg" alt="" />
+              </div>
+            <img className="w-full" src="src/assets/images/1stStep.svg" alt="lab" />
+            </div>
+            <div>
+            <p>{obj.email}</p>
             <p>pirmas</p>
+            </div>
           </div>
         ) : (
-          <div >
+          
+<div className="relative flex items-end w-full justify-center">
+          <div className="absolute flex flex-col w-full max-w-full items-center">
+            <div className="absolute max-w-[60%] max-sm:top-[-30%]">
+            <img className="absolute bottom-[-10%] left-[35%]" src="src/assets/images/thirdPL.svg" alt="" />
+          <img className=""  src="src/assets/images/happyWinner.svg" alt="" />
+            </div>
+          <img className="w-4/5" src="src/assets/images/2-3Place.svg" w-full alt="lab" />
+          </div>
+          <div>
             {obj.email}
             <p>antras</p>
+          </div>
           </div>
         )}
       </li>
--- src/components/pages/RegisterPage.jsx
+++ src/components/pages/RegisterPage.jsx
@@ -48,11 +48,16 @@ function RegisterPage() {
 
   return (
     <div className="mt-20 box-border max-sm:mt-10">
-      <h1 className="text-5xl mb-20 max-sm:text-2xl max-sm:mb-10">Register</h1>
+      {/* <h1 className="text-5xl mb-20 text-white max-sm:text-2xl max-sm:mb-10">Register</h1> */}
 
       <div className="flex flex-col items-center">
-        <div className="flex flex-col items-center px-4 pt-2 pb-5 w-80 max-w-1/3 mb-10 bg-black text-white rounded-lg">
+        <div className="flex flex-col items-center px-6 py-10 max-w-1/2 mb-10 bg-white text-grey rounded-[16px]">
           <div className="font-cursive text-white underline pt-3 pb-3 px-5"></div>
+          <img className="absolute top-0" src="src/assets/images/reglog.svg" alt="" />
+          <div className="w-full bg-lightBlue rounded-[16px] p-1 flex justify-between">
+            <button className="px-[45px] text-white py-[13px] max-w-full bg-blue rounded-[16px]">Login</button>
+            <button className="px-[45px] py-[13px] max-w-full rounded-[16px] text-blue">Register</button>
+          </div>
           <RegisterForm onReg={registrationFireBaseHook} />
         </div>
       </div>
